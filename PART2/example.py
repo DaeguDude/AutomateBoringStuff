184943c040b537bc49f099a4a54d57e737f22e46
SangHak <k3hppk@gmail.com>
2020-05-12 17:17:05 +0900

    05/12 Flow Control

---
+++ PART2/example.py
@@ -0,0 +1,17 @@
+# name = 'Mary'
+# password = 'swordfish'
+# if name == 'Mary':
+#     print('Hello, Mary')
+#     if password == 'swordfish':
+#         print('Access granted.')
+#     else:
+#         print('Wrong password.')
+
+name = 'Mary'
+if name == 'Alice':
+    print('Hi, Alice')
+elif age < 12:
+    print('You are not Alice, Kiddo.')
+else:
+    print('Hello, stranger')
+    
