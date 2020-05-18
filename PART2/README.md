7e02f844ccb6776f296c0d30aa32efc77c96da07
Daegudude <k3hppk@gmail.com>
2020-05-18 16:27:34 +0900

    Function Done

--- PART2/README.md
+++ PART2/README.md
@@ -1,1 +1,7 @@
 # AutomateBoringStuff
+
+1. What are two values of the Boolean data type? How do you write them?
+- True, False
+
+2. What are the three Boolean operators?
+- 
---
+++ PART3/zigzag.py
@@ -0,0 +1,26 @@
+import time, sys
+indent = 0
+indentIncreasing = True 
+
+try:
+  while True:
+    print(' ' * indent, end='')
+    print('********')
+    time.sleep(0.1) # pause for 1/10 of a second
+    
+    if indentIncreasing:
+      # Increase the number of spaces:
+      indent = indent + 1
+      if indent == 20:
+        # Change direction:
+        indentIncreasing = False
+        
+    else:
+      # Decrease the number of spaces:
+      indent = indent - 1
+      if indent == 0:
+        # Change direction:
+        indentIncreasing = True 
+except KeyboardInterrupt:
+  sys.exit()        
+        
